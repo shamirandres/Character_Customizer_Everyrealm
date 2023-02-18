1fe4fa014a910722221bfefd255e7b2d1fed2f61
shamirandres <74631262+shamirandres@users.noreply.github.com>
2023-02-18 14:27:35 -0600

    Bigger changes
    Updated Unity Editor version to 2022.2.7f1
    
    Updated HDRP, Shaders
    
    CC_UI_Manager needs changes

--- CE/Assets/CharacterCustomizer/UI/Scripts/CC_UI_Manager.cs
+++ CE/Assets/CharacterCustomizer/UI/Scripts/CC_UI_Manager.cs
@@ -6,80 +6,64 @@ namespace CC
 {
     public class CC_UI_Manager : MonoBehaviour
     {
-        public static CC_UI_Manager instance; // static reference to the UI manager instance
+        public static CC_UI_Manager instance;
 
         [Tooltip("The parent object of your customizable characters")]
-        public GameObject CharacterParent; // a reference to the parent object of the customizable characters
+        public GameObject CharacterParent;
 
-        public List<AudioClip> UISounds = new List<AudioClip>(); // a list of audio clips to play for UI sounds
+        public List<AudioClip> UISounds = new List<AudioClip>();
 
-        private int characterIndex = 0; // the current index of the active character
+        private int characterIndex = 0;
 
         private void Awake()
         {
-            if (instance == null) // if no UI manager instance exists
+            if (instance == null)
             {
-                instance = this; // set this as the UI manager instance
+                instance = this;
             }
-            else // otherwise, if a UI manager instance already exists
+            else
             {
-                Destroy(gameObject); // destroy this object to ensure only one UI manager instance exists
+                Destroy(gameObject);
             }
         }
 
         private void Start()
         {
-            SetActiveCharacter(0); // set the first character as active on start
-            Invoke("characterNext", 0.1f);
+            SetActiveCharacter(0);
         }
 
         public void playUIAudio(int index)
         {
-            if (UISounds.Count > index) // if the index is within the range of the UISounds list
+            if (UISounds.Count > index)
             {
-                var audioSource = gameObject.GetComponent<AudioSource>(); // get the audio source component on this object
-                audioSource.clip = UISounds[index]; // set the audio clip to the one at the given index
-                audioSource.Play(); // play the audio clip
+                var audioSource = gameObject.GetComponent<AudioSource>();
+                audioSource.clip = UISounds[index];
+                audioSource.Play();
             }
         }
 
         public void SetActiveCharacter(int index)
         {
-            characterIndex = index; // set the current index to the given index
+            characterIndex = index;
 
-            foreach (Transform child in CharacterParent.transform) // iterate over all the child objects of the CharacterParent object
+            for (int i = 0; i < CharacterParent.transform.childCount; i++)
             {
-                var character = child.gameObject; // get the child object as a game object
+                var character = CharacterParent.transform.GetChild(i).gameObject;
                 // Set character active state
-                character.SetActive(child.GetSiblingIndex() == index && index != 0); // set the active state of the character object based on whether its index is equal to the current index
+                character.SetActive(i == index);
                 // Fetch menu from the character's component and set active state
-                character.GetComponent<CharacterCustomization>().UI.SetActive(child.GetSiblingIndex() == index && index != 0); // set the active state of the UI object associated with the character based on whether its index is equal to the current index
+                character.GetComponent<CharacterCustomization>().UI.SetActive(i == index);
             }
-
         }
 
         public void characterNext()
         {
-            int value = (characterIndex + 1) % CharacterParent.transform.childCount;
-            if (value == 0) {
-                SetActiveCharacter(1);
-            }
-            else {
-                SetActiveCharacter(value); // set the active character to the next character by incrementing the index (with wraparound)
-            }
-            
+            SetActiveCharacter((characterIndex + 1) % CharacterParent.transform.childCount);
         }
 
         public void characterPrev()
         {
-            int value = ((characterIndex - 1) + CharacterParent.transform.childCount) % CharacterParent.transform.childCount;
-            if (value == 0)
-            {
-                SetActiveCharacter(CharacterParent.transform.childCount-1);
-            }
-            else { 
-                SetActiveCharacter(value); // set the active character to the previous character by decrementing the index (with wraparound)
-            }
+            SetActiveCharacter((characterIndex - 1 + CharacterParent.transform.childCount) % CharacterParent.transform.childCount);
         }
     }
 }
